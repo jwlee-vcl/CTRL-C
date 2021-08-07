6da69cd9be66506c9e3937b314fde1624dd7e277
Ubuntu <ubuntu@ip-172-31-41-212.ec2.internal>
2021-08-07 12:32:00 +0000

    holicity dataset

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -4,6 +4,7 @@ from datasets.sun360_dataset import build_sun360
 from datasets.yud_dataset import build_yud
 from datasets.ecd_dataset import build_ecd
 from datasets.kitti_dataset import build_kitti
+from datasets.holicity_dataset import build_holicity
 
 def build_dataset(image_set, cfg):
     return build_gsv(image_set, cfg)
@@ -22,4 +23,7 @@ def build_ecd_dataset(image_set, cfg):
 
 def build_kitti_dataset(image_set, cfg):
     return build_kitti(image_set, cfg)
+
+def build_holicity_dataset(image_set, cfg):
+    return build_holicity(image_set, cfg)
     
